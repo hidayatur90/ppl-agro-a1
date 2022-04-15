c7e855732af621ad9385d0a8eabd137d15d0f505
RioAdistya <rioputra191101@gmail.com>
2022-04-15 10:57:18 +0700

    tambah

---
+++ resources/views/produksi/stockKopiTambah.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.app')
+@section('content')
+@include('partials.navbar')
+@include('partials.sidebarProduksi')
+<div id="layoutSidenav_content">
+    <style>
+        strong, ::placeholder {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
+    <main>
+        <div class="container-fluid px-4 py-4">
+            <div class="header">
+                <h4><strong>Tambah Data Produk</strong></h4>
+            </div>
+            <hr>
+            <div class="form-edit">
+                <form method="post" action="/stockKopi/store">
+                    {{ csrf_field() }}
+                    {{ method_field('PATCH') }}
+                    {{-- Nama Produk --}}
+                    <div class="row mb-3">
+                        <label for="nama" class="col-form-label col-sm-4 col-md-3 col-xl-2"><strong>Nama Produk</strong></label>
+                        <div class="col-sm-8 col-md-9 col-xl-10">
+                            <input type="text" id="namaProduk" list="listNama" class="form-control" name="namaProduk" placeholder="Nama Produk" autocomplete="off" required oninvalid="this.setCustomValidity('Nama tidak boleh Kosong')" oninput="this.setCustomValidity('')"/>
+                            <datalist id="listNama" >
+                                @foreach ($produk as $p)
+                                    <option>{{ $p->namaProduk }}</option>
+                                @endforeach
+                            </datalist>
+                            @if($errors->has('namaProduk'))
+                            <div class="text-danger">
+                                {{ $errors->first('namaProduk')}}
+                            </div>
+                        @endif
+                        </div>
+                    </div>
+
+                    {{-- Kategori --}}
+                    <div class="row mb-3">
+                        <label for="kategori" class="col-form-label col-sm-4 col-md-3 col-xl-2"><strong>Kategori</strong></label>
+                        <div class="col-sm-8 col-md-9 col-xl-10">
+                            <select name="kategori" class="form-control" >
+                                <option>Biji Kopi</option>
+                            </select>
+                        </div>
+                    </div>
+                    {{-- Stok --}}
+                    {{-- <div class="row mb-3">
+                        <label for="stok" class="col-form-label col-sm-4 col-md-3 col-xl-2"><strong>Jumlah Stok</strong></label>
+                        <div class="col-sm-8 col-md-9 col-xl-10">
+                            <input type="number" class="form-control" name="jumlahStok" id="jumlahStok" placeholder="Stok Kopi" autocomplete="off" required oninvalid="this.setCustomValidity('Stok harus angka')" oninput="this.setCustomValidity('')"/>
+                            @if($errors->has('jumlahStok'))
+                            <div class="text-danger">
+                                {{ $errors->first('jumlahStok')}}
+                            </div>
+                        @endif
+                        </div>
+                    </div>
+                     --}}
+                    {{-- Kategori --}}
+                    <div class="row mb-3">
+                        <label for="kategori" class="col-form-label col-sm-4 col-md-3 col-xl-2"><strong></strong></label>
+                        <div class="col-sm-8 col-md-9 col-xl-10">
+                            <select name="kategori" class="form-control">
+                                <option>Kopi Bubuk</option>
+                            </select>
+                        </div>
+                    </div>
+                    {{-- Stok --}}
+                    {{-- <div class="row mb-3">
+                        <label for="stok" class="col-form-label col-sm-4 col-md-3 col-xl-2"><strong>Jumlah Stok</strong></label>
+                        <div class="col-sm-8 col-md-9 col-xl-10">
+                            <input type="number" class="form-control" name="jumlahStok" id="jumlahStok" placeholder="Stok Kopi" autocomplete="off" required oninvalid="this.setCustomValidity('Stok harus angka')" oninput="this.setCustomValidity('')"/>
+                            @if($errors->has('jumlahStok'))
+                            <div class="text-danger">
+                                {{ $errors->first('jumlahStok')}}
+                            </div>
+                        @endif
+                        </div>
+                    </div> --}}
+
+
+                    <div class="row mb-3 justify-content-end mx-3 my-4">
+                        <div class="col-sm-8 col-md-9 col-xl-10" style="text-align:end;">
+                            <input type="submit" class="btn btn-success mx-3" value="Tambah">
+                            <a type="button" class="btn btn-secondary border" href="/produksiStockKopi">
+                                Batal
+                            </a>
+                        </div>
+                    </div>
+
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                </form>
+            </div>
+        </div>
+    </main>
+@endsection
